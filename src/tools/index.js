6c35c4f96013c7be6bca548ae08143309d03a071
Peet <64318024+wwhtrbbtt@users.noreply.github.com>
2023-11-07 17:14:52 +0100

    Update index.js

--- src/tools/index.js
+++ src/tools/index.js
@@ -32,7 +32,7 @@ const tools = [
   {
     name: "rot13",
     title: "Rot13 decoder",
-    subtitle: "A utility for encoding/decoding Rot13-encrypted text",
+    subtitle: "An utility for encoding/decoding Rot13 encrypted text",
     func: (i) =>
       i.replace(/[a-z]/gi, (letter) =>
         String.fromCharCode(
@@ -43,14 +43,14 @@ const tools = [
   {
     name: "json_formatter",
     title: "JSON formatter",
-    subtitle: "A utility for formatting/verifying ugly JSON",
+    subtitle: "An utility for formatting/verifiying ugly JSON",
     func: (i) => JSON.stringify(JSON.parse(i), null, "\t"),
     similar: ["js_formatter"],
   },
   {
     name: "js_formatter",
     title: "JavaScript formatter",
-    subtitle: "A utility for formatting/verifying ugly JavaScript",
+    subtitle: "An utility for formatting/verifiying ugly JavaScript",
     func: (i) => js_beautify(i, { indent_size: 2, space_in_empty_paren: true }),
     similar: ["json_formatter"],
   },
@@ -58,7 +58,7 @@ const tools = [
     name: "uuid_analyzer",
     title: "UUID analyzer",
     subtitle:
-      "A utility for extracting all information from a UUID (timestamp + version)",
+      "An utility for getting all information out of an UUID (timestamp + version)",
     similar: ["jwt_decoder"],
     func: (i) => {
       if (i.length !== 36) return "Invalid UUID";
@@ -72,7 +72,7 @@ const tools = [
   {
     name: "jwt_decoder",
     title: "JWT decoder",
-    subtitle: "A utility for decoding a JSON Web Token (JWT)",
+    subtitle: "An utility for decoding a JSON Web Token (JWT)",
     similar: ["uuid_analyzer"],
     func: (i) => {
       const meta = JSON.stringify(
@@ -92,7 +92,7 @@ const tools = [
     name: "header_formatter",
     title: "Header to code",
     subtitle:
-      "A utility for converting raw HTTP headers (for example, from Charles) to a JSON object",
+      "An utility for converting raw HTTP headers (for example from charles) to a JSON object",
     similar: ["uuid_analyzer", "jwt_decoder"],
     func: (i) =>
       JSON.stringify(
